caf800675b2f805735282dcb7d9c790ae99d75b2
Dexter Vorbe <dextervorbe@ymail.com>
2023-02-03 03:39:57 -0500

    Application Set up / Hero Banner created
    Removed files/ created files for project set up
    
    - Created Hero Banner and welcome caption

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import hero from "./images/hero.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header container">
+        {/* <img src={hero}></img> */}
+        <div>
+          <h1 className="textLogo">RoosterGrin</h1>
+        </div>
+
+        <div className="greetingBox">
+          <h2>
+            Welcome To <br></br>
+            <span>Rooster Grin</span>
+          </h2>
+          <button></button>
+        </div>
+      </div>
+
+      {/* <div className="container">
+        <p></p>
+      </div> */}
     </div>
   );
 }
